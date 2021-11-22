3216f36ae241ba1e28e8eede1d2c1ac5247cbd30
Caio Sousa <caiofersousa@hotmail.com>
2021-11-22 09:49:22 -0300

    First rout with nodejs

--- src/index.js
+++ src/index.js
@@ -7,4 +7,7 @@ const app = express()
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.listen(3000)
+app.get('/', (req, res)=>{
+    res.send('OK');
+})
+app.listen(3000);
